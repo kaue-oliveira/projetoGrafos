a45c84f66c6c64668dbb2ada57313ca1cfe16e20
kaue <kaueolivsil@gmail.com>
2024-05-16 22:09:09 -0300

    mudanças no menu para melhor visualização

--- mainGrafo.cpp
+++ mainGrafo.cpp
@@ -1,12 +1,20 @@
 #include <iostream>
 #include <fstream>
+#include <unistd.h> // Para o usleep
 #include "grafo.h"
 #include "filadinamica.h"
 #include "pilhadinamica.h"
+
 using namespace std;
 
 void exibirMenu() {
-    cout << "====== Menu ======" << endl;
+    cout << "\033[2J\033[1;1H"; 
+    cout << "\033[1m\033[33m"; 
+    cout << "====================================================" << endl;
+    cout << "|                     M E N U                      |" << endl;
+    cout << "====================================================" << endl;
+    cout << "\033[0m";
+    cout << "\033[1m\033[34m";
     cout << "1. Inserir vértice" << endl;
     cout << "2. Inserir aresta" << endl;
     cout << "3. Obter grau de um vértice" << endl;
@@ -18,7 +26,25 @@ void exibirMenu() {
     cout << "9. Busca em largura" << endl;
     cout << "10. Busca em profundidade" << endl;
     cout << "11. Sair" << endl;
-    cout << "===================" << endl;
+    cout << "\033[0m"; 
+    cout << "\033[1m\033[33m";
+    cout << "====================================================" << endl;
+    cout << "\033[0m";
+    cout << "Escolha uma opção: ";
+}
+
+void limpaTela() {
+    cout << "\033[2J\033[1;1H";
+}
+
+void animacaoTransicao() {
+    limpaTela();
+    cout << "\033[1m\033[32m";
+    cout << "====================================================" << endl;
+    cout << "|                   Carregando...                   |" << endl;
+    cout << "====================================================" << endl;
+    cout << "\033[0m";
+    usleep(1000000); // 1 segundo
 }
 
 int main() {
@@ -27,7 +53,7 @@ int main() {
     string item1, item2;
     Grafo grafo(numVertices);
 
-    // Pergunta se o grafo é direcionado
+    
     char resposta;
     cout << "O grafo é direcionado? (y/n): ";
     cin >> resposta;
@@ -38,20 +64,26 @@ int main() {
     grafo.reescreverArquivo();
 
     do {
+        limpaTela();
         exibirMenu();
-        cout << "Escolha uma opção: ";
         cin >> opcao;
 
         switch (opcao) {
             case 1: {
+                animacaoTransicao();
                 string vertice;
                 cout << "Digite o nome do vértice a ser inserido: ";
                 cin >> vertice;
                 grafo.inserevertice(vertice);
                 grafo.reescreverArquivo();
+                
+                cout << "Pressione qualquer tecla para voltar ao menu principal...";
+                cin.ignore();
+                cin.get();
                 break;
             }
             case 2: {
+                animacaoTransicao();
                 string saida, entrada;
                 cout << "Digite o nome do vértice de saída: ";
                 cin >> saida;
@@ -59,61 +91,108 @@ int main() {
                 cin >> entrada;
                 grafo.inserearesta(saida, entrada);
                 grafo.reescreverArquivo();
+                
+                cout << "Pressione qualquer tecla para voltar ao menu principal...";
+                cin.ignore();
+                cin.get();
                 break;
             }
             case 3: {
+                animacaoTransicao();
                 string vertice;
                 cout << "Digite o nome do vértice para obter o grau: ";
                 cin >> vertice;
                 cout << "Grau do vértice: " << grafo.obtergrau(vertice) << endl;
+                
+                cout << "Pressione qualquer tecla para voltar ao menu principal...";
+                cin.ignore();
+                cin.get();
                 break;
             }
-            case 4:
+            case 4: {
+                animacaoTransicao();
                 grafo.imprimirmatriz();
+                
+                cout << "Pressione qualquer tecla para voltar ao menu principal...";
+                cin.ignore();
+                cin.get();
                 break;
-            case 5:
+            }
+            case 5: {
+                animacaoTransicao();
                 grafo.imprimirvertices();
+                
+                cout << "Pressione qualquer tecla para voltar ao menu principal...";
+                cin.ignore();
+                cin.get();
                 break;
+            }
             case 6: {
+                animacaoTransicao();
                 string verticeRemover;
                 cout << "Digite o nome do vértice a ser removido: ";
                 cin >> verticeRemover;
                 grafo.removervertice(verticeRemover);
                 grafo.reescreverArquivo();
+                
+                cout << "Pressione qualquer tecla para voltar ao menu principal...";
+                cin.ignore();
+                cin.get();
                 break;
             }
             case 7: {
+                animacaoTransicao();
                 cout << "A quantidade de vertices no grafo é: ";
                 cout << grafo.qtdvertice() << endl;
+                
+                cout << "Pressione qualquer tecla para voltar ao menu principal...";
+                cin.ignore();
+                cin.get();
                 break;
             }
             case 8: {
+                animacaoTransicao();
                 cout << "A quantidade de arestas no grafo é: ";
                 cout << grafo.qtdarestas() << endl;
+                
+                cout << "Pressione qualquer tecla para voltar ao menu principal...";
+                cin.ignore();
+                cin.get();
                 break;
             }
             case 9: {
+                animacaoTransicao();
                 cout << "Digite o vertice de origem: ";
                 cin >> item1;
                 cout << "Digite o vertice de destino: ";
                 cin >> item2;
                 grafo.buscaemlargura(item1, item2);
+                
+                cout << "Pressione qualquer tecla para voltar ao menu principal...";
+                cin.ignore();
+                cin.get();
                 break;
             }
             case 10: {
+                animacaoTransicao();
                 cout << "Digite o vertice de origem: ";
                 cin >> item1;
                 cout << "Digite o vertice de destino: ";
                 cin >> item2;
                 grafo.buscaemprofundidade(item1, item2);
+                
+                cout << "Pressione qualquer tecla para voltar ao menu principal...";
+                cin.ignore();
+                cin.get();
                 break;
             }
             case 11:
-                cout << "Saindo..." << endl;
+                cout << "Obrigado por usar o nosso sitema =)" << endl;
                 break;
             default:
                 cout << "Opção inválida!" << endl;
         }
+        usleep(500000); // Aguarda 5.5 segundos para dar tempo de ler antes de limpar a tela
     } while (opcao != 11);
     
     grafo.reescreverArquivo();
